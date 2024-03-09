85e51667f36fe20debfce139882b47538f91f5d1
ravisenevirathne <ravi2005@gmail.com>
2024-03-10 00:22:00 +1100

    updated

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
+
+
+app.use(express.json())
 
 //routes
 app.get('/',(req, res) => {
@@ -7,7 +12,86 @@ app.get('/',(req, res) => {
 }
 )
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+app.get('/blog',(req, res) => {
+    res.send('Hello from Blog')
 }
 )
+
+app.get('/products', async(req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get('/products/:id', async(req,res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+
+    }catch (error){
+        res.status(500).json({message: error.message});
+    }
+
+}
+)
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+}
+)
+
+app.put('/products/:id', async(req,res) => {
+    try {
+        const  {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product) {
+            return res.status(404).json({message: 'cannot find product with ID ${id}'})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch {
+        res.status(500).json({message: error.message})
+    }
+
+}
+)
+
+app.delete('/products/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        if (!product) {
+            return res.status(404).json({message: 'cannot find product with ID ${id}'})
+        }
+        res.status(200).json(product);
+
+    } catch {
+        res.status(500).json({message: error.message});
+}
+}
+)
+
+
+mongoose.connect('mongodb+srv://admin:admin@ravisenevirathne.rbsrrlr.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ravisenevirathne')
+.then(()=> {
+    console.log('Connected to DB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    }
+    )
+
+}) .catch((error) => {
+    console.log(error)
+})
